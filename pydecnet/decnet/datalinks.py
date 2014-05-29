51f7d7a1216eb22c0d2b25a7e16d2f669ef567d9
Paul Koning <ni1d@arrl.net>
2014-05-29 18:25:51 +0000

    Split out the individual datalink implementations to separate files
    git-svn-id: svn://akdesign.dyndns.org/pydecnet/trunk@114 02bf3f84-b523-46f3-a765-a93b7296b289

---
+++ pydecnet/decnet/datalinks.py
@@ -0,0 +1,13 @@
+#!
+
+"""Classes for the datalink layer as used by DECnet routing.
+
+"""
+
+# Point to point
+from . import simdmc
+from . import multinet
+
+# Broadcast
+from . import ethernet
+from . import gre
