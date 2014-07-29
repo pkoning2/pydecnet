d25b94aa91712ce49418e8c81beb00ff4e8f4a74
Paul Koning <ni1d@arrl.net>
2014-07-29 19:34:12 +0000

    First bits of an event logger component
    git-svn-id: svn://akdesign.dyndns.org/pydecnet/trunk/pydecnet@224 02bf3f84-b523-46f3-a765-a93b7296b289

--- decnet/nice.py
+++ decnet/nice.py
@@ -321,7 +321,270 @@ def fmtparamclass (fmt):
             # DS-n
             return SIntParam
         return UIntParam
-    
+
+class Counter (object):
+    """Base class for DNA counters.
+    """
+    _code = None
+    _width = None
+    _bits = ( )
+
+"""
+                     Circuit Counters Kept for All Circuits
+
+        Type    Bit
+       Number  Width        Standard Text
+
+            0   16    Seconds Since Last Zeroed
+          800   32    Terminating Packets Received
+          801   32    Originating Packets Sent
+          802   16    Terminating Congestion Loss
+          805    8    Corruption Loss
+          810   32    Transit Packets Received
+          811   32    Transit Packets Sent
+          812   16    Transit Congestion Loss
+          820    8    Circuit Down
+          821    8    Initialization Failure
+
+
+     The following Data Link counters apply to DDCMP circuits:
+
+
+                                    Table 7
+                 Data Link Circuit Counters for DDCMP Circuits
+
+      Type      Bit                            Bit Number
+     Number    Width     Standard Text           Standard Text
+
+      1000      32    Bytes Received
+      1001      32    Bytes Sent
+      1010      32    Data Blocks Received
+      1011      32    Data Blocks Sent
+      1020       8    Data Errors Inbound      1 NAKs Sent, Data Field
+                                                  Block Check error
+                                               2 NAKs Sent, REP Response
+      1021       8    Data Errors Outbound     0 NAKs Received, Header
+                                                  Block Check error
+                                               1 NAKs Received, Data Field
+                                                  Block Check error
+                                               2 NAKs Received, REP
+                                                  Response
+      1030       8    Remote Reply Timeouts
+      1031       8    Local Reply Timeouts
+      1040       8    Remote Buffer Errors     0 NAKs Received Buffer
+                                                  Unavailable
+                                               1 NAKs Received Buffer Too
+                                                  Small
+      1041       8    Local Buffer Errors      0 NAKs Sent Buffer
+                                                  Unavailable
+                                               1 NAKs Sent Buffer Too
+                                                  Small
+      1050      16    Selection Intervals
+                       Elapsed
+      1051       8    Selection Timeouts       0 No Reply to Select
+                                               1 Incomplete Reply to
+                                                  Select
+     Parameter and Counter Binary Formats and Values
+
+
+     The following Data Link counters apply to permanent X.25 circuits:
+
+
+                                    Table 8
+             Data Link Circuit Counters for Permanent X.25 Circuits
+
+        Type    Bit
+       Number  Width     Standard Text
+
+        1000    32       Bytes received
+        1001    32       Bytes sent
+        1010    32       Data blocks received
+        1011    32       Data blocks sent
+        1240     8       Locally initiated resets
+        1241     8       Remotely initiated resets
+        1242     8       Network initiated resets
+
+
+     The  following  table  specifies  Data  Link  counters  for   Ethernet
+     circuits:
+
+
+                                    Table 9
+                Data Link Circuit Counters for Ethernet Circuits
+
+        Type    Bit
+       Number  Width     Standard Text
+
+           0    16       Seconds Since Last Zeroed
+        1000    32       Bytes Received
+        1001    32       Bytes Sent
+        1010    32       Data Blocks Received
+        1011    32       Data Blocks Sent
+        1065    16       User buffer unavailable
+
+                     Data Link Line Counters for LAPB Lines
+
+        Type    Bit                            Bit Number
+       Number  Width     Standard Text           Standard Text
+
+           0    16    Seconds Since Last Zeroed
+        1000    32    Bytes Received
+        1001    32    Bytes Sent
+        1010    32    Data Blocks Received
+        1011    32    Data Blocks Sent
+        1020     8    Data Errors Inbound      3 Block too long
+                                               4 Block check error
+                                               5 REJ sent
+        1021     8    Data Errors Outbound     3 REJ received
+        1030     8    Remote Reply Timeouts
+        1031     8    Local Reply Timeouts
+        1040     8    Remote Buffer Errors     2 RNR received, buffer
+                                                 unavailable
+        1041     8    Local Buffer Errors      2 RNR sent, buffer
+                                                 unavailable
+        1100     8    Remote Station Errors    4 Invalid N(R) received
+                                               5 FRMR sent, header
+                                                 format error
+        1101     8    Local Station Errors     2 Transmit underrun
+                                               4 Receive overrun
+                                               5 FRMR received, head
+                                                 format error
+
+
+     The following table specifies Data Link counters for DDCMP lines:
+
+
+                                    Table 13
+                    Data Link Line Counters for DDCMP Lines
+
+        Type    Bit                            Bit Number
+       Number  Width     Standard Text           Standard Text
+
+        1020    8     Data Errors Inbound      0 NAKs sent, header block
+                                                 check error
+        1100    8     Remote Station Errors    0 NAKs received, receive
+                                                 overrun
+                                               1 NAKs sent, header
+                                                 format error
+                                               2 Selection address
+                                                 errors
+                                               3 Streaming tributaries
+        1101    8     Local Station Errors     0 NAKs sent, receive
+                                                 overrun
+                                               1 Receive overruns, NAK
+                                                 not sent
+                                               2 Transmit underruns
+                                               3 NAKs received, header
+                                                 format error
+     Parameter and Counter Binary Formats and Values
+
+
+     The following table specifies Data Link counters for Ethernet lines:
+
+
+                                   Table 13.1
+                   Data Link Line Counters for Ethernet Lines
+
+        Type    Bit                            Bit Number
+       Number  Width     Standard Text           Standard Text
+
+           0    16    Seconds Since Last Zeroed
+        1000    32    Bytes Received
+        1001    32    Bytes Sent
+        1002    32    Multicast Bytes Received
+        1010    32    Data Blocks Received
+        1011    32    Data Blocks Sent
+        1012    32    Multicast Blocks Received
+        1013    32    Blocks sent, initially deferred
+        1014    32    Blocks sent, single collision
+        1015    32    Blocks sent, multiple collisions
+        1060    16    Send failure             0 Excessive collisions
+                                               1 Carrier check failed
+                                               2 Short circuit
+                                               3 Open circuit
+                                               4 Frame too long
+                                               5 Remote failure to defer
+        1061    16    Collision detect check failure
+        1062    16    Receive failure          0 Block check error
+                                               1 Framing error
+                                               2 Frame too long
+        1063    16    Unrecognized frame destination
+        1064    16    Data overrun
+        1065    16    System buffer unavailable
+        1066    16    User buffer unavailable
+                                    Table 18
+                         X.25 Protocol Module Counters
+
+      Type       Bit
+     Number     Width         Standard Text
+
+        0        16     Seconds since last zeroed
+     1000        32     Bytes received
+     1001        32     Bytes sent
+     1010        32     Data blocks received
+     1011        32     Data blocks sent
+     1200        16     Calls received
+     1201        16     Calls sent
+     1210        16     Fast selects received
+     1211        16     Fast selects sent
+     1220        16     Maximum switched circuits active
+     1221        16     Maximum channels active
+     1230        16     Received call resource errors
+     1240         8     Locally initiated resets
+     1241         8     Remotely initiated resets
+     1242         8     Network initiated resets
+     1250         8     Restarts
+
+
+
+            X.25 Server Module Counters
+
+     The following table specifies the X.25 server module counters.
+
+
+                                    Table 19
+                          X.25 Server Module Counters
+
+        Type   Bit
+       Number  Width     Standard Text
+
+           0    16    Seconds since last zeroed
+         200    16    Maximum circuits active
+         210     8    Incoming calls rejected, no resources
+         211     8    Logical links rejected, no resources
+
+
+
+  Node counters:
+       Appl.   Type Number  Bit width        Standard Text
+
+        DN         0           16       Seconds Since Last Zeroed
+
+        DN       600           32       User Bytes Received
+        DN       601           32       User Bytes Sent
+        DN       602           32       User Messages Received
+        DN       603           32       User Messages Sent
+        DN       608           32       Total Bytes Received
+        DN       609           32       Total Bytes Sent
+        DN       610           32       Total Messages Received
+        DN       611           32       Total Messages Sent
+        DN       620           16       Connects Received
+        DN       621           16       Connects Sent
+        DN       630           16       Response Timeouts
+        DN       640           16       Received Connect Resource Errors
+        E        700           16       Maximum Logical Links Active
+     Parameter and Counter Binary Formats and Values
+
+
+        E        900            8       Aged Packet Loss
+        E        901           16       Node Unreachable Packet Loss
+        E        902            8       Node Out-of-Range Packet Loss
+        E        903            8       Oversized Packet Loss
+        E        910            8       Packet Format Error
+        E        920            8       Partial Routing Update Loss
+        E        930            8       Verification Reject
+
+"""
 class nicemsg_meta (type):
     def __new__ (cls, name, bases, classdict):
         pdict = dict ()
