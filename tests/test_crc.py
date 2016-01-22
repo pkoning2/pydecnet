7939690caf861aef7c4abca14d0dedab2c72f1f1
Paul Koning <ni1d@arrl.net>
2016-01-22 20:08:35 +0000

    Generate the test classes in a more elegant way
    git-svn-id: svn://akdesign.dyndns.org/pydecnet/trunk/pydecnet@270 02bf3f84-b523-46f3-a765-a93b7296b289

--- tests/test_crc.py
+++ tests/test_crc.py
@@ -8,6 +8,54 @@ import crc
     
 reverse = crc._reverse
 
+# This table lists the parameters for all the CRCs given in the Wikipedia
+# article, except for one whose length is unclear, plus one more taken
+# from http://mathforum.org/kb/thread.jspa?threadID=508110&messageID=1553817
+# It is used below to generate the test case classes for each of these.
+generators = (( 1, "Parity", 0x1 ),
+              ( 4, "ITU", 0x3 ),
+              ( 5, "EPC", 0x9 ),
+              ( 5, "ITU", 0x15 ),
+              ( 5, "USB", 0x5 ),
+              ( 6, "CDMA2000_A", 0x27 ),
+              ( 6, "CDMA2000_B", 0x7 ),
+              ( 6, "ITU", 0x3 ),
+              ( 7, "", 0x9 ),
+              ( 7, "MVB", 0x65 ),
+              ( 8, "", 0xab ),
+              ( 8, "CCITT", 0x7 ),
+              ( 8, "Dallas", 0x31 ),
+              ( 8, "SAE_J1850", 0x1d ),
+              ( 8, "WCDMA", 0x9b ),
+              ( 10, "", 0x233 ),
+              ( 10, "CDMA2000", 0x3d9 ),
+              ( 11, "", 0x385 ),
+              ( 12, "", 0x80f ),
+              ( 12, "CDMA2000", 0xf13 ),
+              ( 13, "BBC", 0x1cf5 ),
+              ( 15, "CAN", 0x4599 ),
+              ( 15, "MPT1327", 0x6815 ),
+              ( 16, "ARINC", 0xa02b ),
+              ( 16, "CCITT", 0x1021 ),
+              ( 16, "CDMA2000", 0xc867 ),
+              ( 16, "DECT", 0x589 ),
+              ( 16, "T10_DIF", 0x8bb7 ),
+              ( 16, "DNP", 0x3d65 ),
+              ( 16, "IBM", 0x8005 ),
+              ( 17, "CAN", 0x1685b ),
+              ( 21, "CAN", 0x102899 ),
+              ( 24, "", 0x5d6dcb ),
+              ( 24, "Radix_64", 0x864cfb ),
+              ( 30, "", 0x2030b9c7 ),
+              ( 32, "", 0x4c11db7 ),
+              ( 32, "C", 0x1edc6f41 ),
+              ( 32, "K", 0xeb31d82e ),
+              ( 32, "Q", 0x814141ab ),
+              ( 40, "GSM", 0x4820009 ),
+              ( 64, "ECMA", 0x42f0e1eba9ea3693 ),
+              ( 64, "ISO", 0x1b ),
+              ( 128, "Zimmerman", 0x9b9346606fb953db45da2af80fb518cf ))
+
 def randomdata (n, minlen, maxlen, step = 1):
     """Yield n pairs of length, value, where length is a random number
     from range (minlen, maxlen, step) and value is a random integer
@@ -59,10 +107,9 @@ class crctestbase (DnTest):
     """
     @classmethod
     def setUpClass (cls):
-        cls.initial = ini = random.getrandbits (cls.width)
-        cls.final = fin = random.getrandbits (cls.width)
         class testcrc (crc.CRC, poly = cls.poly, width = cls.width,
-                       initial = ini, final = fin, reversed = cls.reversed):
+                       initial = cls.initial, final = cls.final,
+                       reversed = cls.reversed):
             pass
         cls.cls = testcrc
 
@@ -117,8 +164,7 @@ class crctestbase (DnTest):
             self.assertEqual (self.cls.final, self.final)
 
     def test_random_bytes (self):
-        """Check the CRC class against a reference implementation for
-        messages that are an integral multiple of 8 bits in length.
+        """Check the CRC calculation for random byte strings.
         """
         for l, v in randomdata (100, 8, 2000, 8):
             c1 = self.ref_crc (v, l)
@@ -133,8 +179,7 @@ class crctestbase (DnTest):
             self.assertTrue (c.good)
         
     def test_random_bits (self):
-        """Check the CRC class against a reference implementation for
-        messages that are any length (not just multiples of 8).
+        """Check the CRC calculation for random bit strings.
         """
         for l, v in randomdata (100, 8, 2000):
             c1 = self.ref_crc (v, l)
@@ -146,8 +191,7 @@ class crctestbase (DnTest):
             self.assertTrue (c.good)
         
     def test_random_pieces (self):
-        """Check the CRC class against a reference implementation
-        with the data supplies to the class in random length pieces.
+        """Check the CRC calculation done piecemeal in random lengths.
         """
         for l, v in randomdata (100, 8, 2000):
             c1 = self.ref_crc (v, l)
@@ -172,271 +216,26 @@ class crctestbase (DnTest):
             # Verify the "good" check
             c.update_bits (c.value, c.width)
             self.assertTrue (c.good)
-        
-# The following test classes check all the CRCs given in the Wikipedia
-# article  (except for one whose length is unclear).  Both the regular
-# and reversed cases are generated.
-class Test_CRC_1_Parity (crctestbase):
-    poly = 0x1
-    width = 1
-    reversed = False
-class Test_CRC_1_Parity_r (Test_CRC_1_Parity):
-    reversed = True
-class Test_CRC_4_ITU (crctestbase):
-    poly = 0x3
-    width = 4
-    reversed = False
-class Test_CRC_4_ITU_r (Test_CRC_4_ITU):
-    reversed = True
-class Test_CRC_5_EPC (crctestbase):
-    poly = 0x9
-    width = 5
-    reversed = False
-class Test_CRC_5_EPC_r (Test_CRC_5_EPC):
-    reversed = True
-class Test_CRC_5_ITU (crctestbase):
-    poly = 0x15
-    width = 5
-    reversed = False
-class Test_CRC_5_ITU_r (Test_CRC_5_ITU):
-    reversed = True
-class Test_CRC_5_USB (crctestbase):
-    poly = 0x5
-    width = 5
-    reversed = False
-class Test_CRC_5_USB_r (Test_CRC_5_USB):
-    reversed = True
-class Test_CRC_6_CDMA2000_A (crctestbase):
-    poly = 0x27
-    width = 6
-    reversed = False
-class Test_CRC_6_CDMA2000_A_r (Test_CRC_6_CDMA2000_A):
-    reversed = True
-class Test_CRC_6_CDMA2000_B (crctestbase):
-    poly = 0x7
-    width = 6
-    reversed = False
-class Test_CRC_6_CDMA2000_B_r (Test_CRC_6_CDMA2000_B):
-    reversed = True
-class Test_CRC_6_ITU (crctestbase):
-    poly = 0x3
-    width = 6
-    reversed = False
-class Test_CRC_6_ITU_r (Test_CRC_6_ITU):
-    reversed = True
-class Test_CRC_7 (crctestbase):
-    poly = 0x9
-    width = 7
-    reversed = False
-class Test_CRC_7_r (Test_CRC_7):
-    reversed = True
-class Test_CRC_7_MVB (crctestbase):
-    poly = 0x65
-    width = 7
-    reversed = False
-class Test_CRC_7_MVB_r (Test_CRC_7_MVB):
-    reversed = True
-class Test_CRC_8 (crctestbase):
-    poly = 0xab
-    width = 8
-    reversed = False
-class Test_CRC_8_r (Test_CRC_8):
-    reversed = True
-class Test_CRC_8_CCITT (crctestbase):
-    poly = 0x7
-    width = 8
-    reversed = False
-class Test_CRC_8_CCITT_r (Test_CRC_8_CCITT):
-    reversed = True
-class Test_CRC_8_Dallas (crctestbase):
-    poly = 0x31
-    width = 8
-    reversed = False
-class Test_CRC_8_Dallas_r (Test_CRC_8_Dallas):
-    reversed = True
-class Test_CRC_8_SAE_J1850 (crctestbase):
-    poly = 0x1d
-    width = 8
-    reversed = False
-class Test_CRC_8_SAE_J1850_r (Test_CRC_8_SAE_J1850):
-    reversed = True
-class Test_CRC_8_WCDMA (crctestbase):
-    poly = 0x9b
-    width = 8
-    reversed = False
-class Test_CRC_8_WCDMA_r (Test_CRC_8_WCDMA):
-    reversed = True
-class Test_CRC_10 (crctestbase):
-    poly = 0x233
-    width = 10
-    reversed = False
-class Test_CRC_10_r (Test_CRC_10):
-    reversed = True
-class Test_CRC_10_CDMA2000 (crctestbase):
-    poly = 0x3d9
-    width = 10
-    reversed = False
-class Test_CRC_10_CDMA2000_r (Test_CRC_10_CDMA2000):
-    reversed = True
-class Test_CRC_11 (crctestbase):
-    poly = 0x385
-    width = 11
-    reversed = False
-class Test_CRC_11_r (Test_CRC_11):
-    reversed = True
-class Test_CRC_12 (crctestbase):
-    poly = 0x80f
-    width = 12
-    reversed = False
-class Test_CRC_12_r (Test_CRC_12):
-    reversed = True
-class Test_CRC_12_CDMA2000 (crctestbase):
-    poly = 0xf13
-    width = 12
-    reversed = False
-class Test_CRC_12_CDMA2000_r (Test_CRC_12_CDMA2000):
-    reversed = True
-class Test_CRC_13_BBC (crctestbase):
-    poly = 0x1cf5
-    width = 13
-    reversed = False
-class Test_CRC_13_BBC_r (Test_CRC_13_BBC):
-    reversed = True
-class Test_CRC_15_CAN (crctestbase):
-    poly = 0x4599
-    width = 15
-    reversed = False
-class Test_CRC_15_CAN_r (Test_CRC_15_CAN):
-    reversed = True
-class Test_CRC_15_MPT1327 (crctestbase):
-    poly = 0x6815
-    width = 15
-    reversed = False
-class Test_CRC_15_MPT1327_r (Test_CRC_15_MPT1327):
-    reversed = True
-class Test_CRC_16_ARINC (crctestbase):
-    poly = 0xa02b
-    width = 16
-    reversed = False
-class Test_CRC_16_ARINC_r (Test_CRC_16_ARINC):
-    reversed = True
-class Test_CRC_16_CCITT (crctestbase):
-    poly = 0x1021
-    width = 16
-    reversed = False
-class Test_CRC_16_CCITT_r (Test_CRC_16_CCITT):
-    reversed = True
-class Test_CRC_16_CDMA2000 (crctestbase):
-    poly = 0xc867
-    width = 16
-    reversed = False
-class Test_CRC_16_CDMA2000_r (Test_CRC_16_CDMA2000):
-    reversed = True
-class Test_CRC_16_DECT (crctestbase):
-    poly = 0x589
-    width = 16
-    reversed = False
-class Test_CRC_16_DECT_r (Test_CRC_16_DECT):
-    reversed = True
-class Test_CRC_16_T10_DIF (crctestbase):
-    poly = 0x8bb7
-    width = 16
-    reversed = False
-class Test_CRC_16_T10_DIF_r (Test_CRC_16_T10_DIF):
-    reversed = True
-class Test_CRC_16_DNP (crctestbase):
-    poly = 0x3d65
-    width = 16
-    reversed = False
-class Test_CRC_16_DNP_r (Test_CRC_16_DNP):
-    reversed = True
-class Test_CRC_16_IBM (crctestbase):
-    poly = 0x8005
-    width = 16
-    reversed = False
-class Test_CRC_16_IBM_r (Test_CRC_16_IBM):
-    reversed = True
-class Test_CRC_17_CAN (crctestbase):
-    poly = 0x1685b
-    width = 17
-    reversed = False
-class Test_CRC_17_CAN_r (Test_CRC_17_CAN):
-    reversed = True
-class Test_CRC_21_CAN (crctestbase):
-    poly = 0x102899
-    width = 21
-    reversed = False
-class Test_CRC_21_CAN_r (Test_CRC_21_CAN):
-    reversed = True
-class Test_CRC_24 (crctestbase):
-    poly = 0x5d6dcb
-    width = 24
-    reversed = False
-class Test_CRC_24_r (Test_CRC_24):
-    reversed = True
-class Test_CRC_24_Radix_64 (crctestbase):
-    poly = 0x864cfb
-    width = 24
-    reversed = False
-class Test_CRC_24_Radix_64_r (Test_CRC_24_Radix_64):
-    reversed = True
-class Test_CRC_30 (crctestbase):
-    poly = 0x2030b9c7
-    width = 30
-    reversed = False
-class Test_CRC_30_r (Test_CRC_30):
-    reversed = True
-class Test_CRC_32 (crctestbase):
-    poly = 0x4c11db7
-    width = 32
-    reversed = False
-class Test_CRC_32_r (Test_CRC_32):
-    reversed = True
-class Test_CRC_32_C (crctestbase):
-    poly = 0x1edc6f41
-    width = 32
-    reversed = False
-class Test_CRC_32_C_r (Test_CRC_32_C):
-    reversed = True
-class Test_CRC_32_K (crctestbase):
-    poly = 0xeb31d82e
-    width = 32
-    reversed = False
-class Test_CRC_32_K_r (Test_CRC_32_K):
-    reversed = True
-class Test_CRC_32_Q (crctestbase):
-    poly = 0x814141ab
-    width = 32
-    reversed = False
-class Test_CRC_32_Q_r (Test_CRC_32_Q):
-    reversed = True
-class Test_CRC_40_GSM (crctestbase):
-    poly = 0x4820009
-    width = 40
-    reversed = False
-class Test_CRC_40_GSM_r (Test_CRC_40_GSM):
-    reversed = True
-class Test_CRC_64_ECMA (crctestbase):
-    poly = 0x42f0e1eba9ea3693
-    width = 64
-    reversed = False
-class Test_CRC_64_ECMA_r (Test_CRC_64_ECMA):
-    reversed = True
-class Test_CRC_64_ISO (crctestbase):
-    poly = 0x1b
-    width = 64
-    reversed = False
-class Test_CRC_64_ISO_r (Test_CRC_64_ISO):
-    reversed = True
-# These two are from
-# http://mathforum.org/kb/thread.jspa?threadID=508110&messageID=1553817
-class Test_CRC_128_Zimmerman (crctestbase):
-    poly = 0x9b9346606fb953db45da2af80fb518cf
-    width = 128
-    reversed = False
-class Test_CRC_128_Zimmerman_r (Test_CRC_128_Zimmerman):
-    reversed = True
-    
+
+# Generate the specific test cases.  We do this by generating the
+# classes dynamically, and adding them as module level names through
+# the "global" dict.  
+glob = globals ()
+for width, name, poly in generators:
+    if name:
+        name = "Test_CRC_{}_{}".format (width, name)
+    else:
+        name = "Test_CRC_{}".format (width)
+    cdict = dict (initial = random.getrandbits (width),
+                  final = random.getrandbits (width),
+                  poly = poly, width = width, reversed = False)
+    # Generate the "forward" class
+    glob[name] = type (name, (crctestbase,), cdict)
+    # Now the "reverse" class
+    name += "_rev"
+    cdict["reversed"] = True
+    glob[name] = type (name, (crctestbase,), cdict)
+
 if __name__ == "__main__":
     unittest.main ()
     
