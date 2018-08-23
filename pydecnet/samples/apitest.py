abb11d2b57d7198d78276e7e53debd3d86ca1e5a
Paul Koning <ni1d@arrl.net>
2018-08-23 21:18:02 +0000

    Update API sample
    git-svn-id: svn://akdesign.dyndns.org/pydecnet/trunk@289 02bf3f84-b523-46f3-a765-a93b7296b289

--- pydecnet/samples/apitest.py
+++ pydecnet/samples/apitest.py
@@ -1,47 +1,15 @@
-#!/usr/bin/env python3.2
+#!/usr/bin/env python3
 
-import socket
-import io
-import sys
+import urllib.request
+import ssl
+import json
 
-# We need this because socket.makefile does not support the line_buffering
-# argument for text mode.
-def makefile (sock, mode, buf = -1):
-    f = sock.makefile (mode + "b", buffering = buf)
-    f = io.TextIOWrapper (f, encoding = "ascii",
-                          errors = "ignore", newline = None,
-                          line_buffering = True)
-    f.mode = mode
-    return f
-    
-sock = socket.socket (socket.AF_UNIX, socket.SOCK_STREAM)
-args = sys.argv[1:]
-if len (args) > 2 and args[0] == "-s":
-    sockname = args[1]
-    args = args[2:]
-else:
-    sockname = "decnetsocket"
-sock.connect (sockname)
-rf = makefile (sock, "r", buf = 1)
-wf = makefile (sock, "w")
-
-# "quit" argument means break the connection prematurely to test
-# server error handling.
-if args and args[0] == "quit":
-    sys.exit (0)
-
-try:
-    if args:
-        s = ' '.join (args) + '\n'
-    else:
-        s = input ("> ") + '\n'
-    wf.write (s)
-    while True:
-        r = rf.readline ()
-        if not r:
-            break
-        sys.stdout.write (r)
-finally:
-    rf.close ()
-    wf.close ()
-    sock.close ()
+# Read the list of systems
+context = ssl.SSLContext (ssl.PROTOCOL_SSLv23 )
+context.check_hostname = False
+resp = urllib.request.urlopen ("https://127.0.0.1:8443/api", context = context)
+print ("headers:")
+print (" ", "\n  ".join (str (h) for h in resp.getheaders ()))
+print ("list of systems returned by API:")
+ret = json.loads (str (resp.read (), encoding = "latin1"))
+print (" ", "\n  ".join (("  ".join (f for f in l) for l in ret)))
