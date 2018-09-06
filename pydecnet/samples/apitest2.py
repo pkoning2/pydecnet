b011b0796016d04eebbc99ecb6e16a0f5d30ba4b
Paul Koning <ni1d@arrl.net>
2018-09-06 13:59:41 +0000

    Bugfix
    git-svn-id: svn://akdesign.dyndns.org/pydecnet/trunk@307 02bf3f84-b523-46f3-a765-a93b7296b289

--- pydecnet/samples/apitest2.py
+++ pydecnet/samples/apitest2.py
@@ -3,6 +3,9 @@
 # Similar to apitest.py, but using the Requests package
 
 import requests
+import warnings
+
+warnings.simplefilter ("ignore")
 
 # Read the list of systems.  verify is False because my test certificate
 # isn't completely correct, which is fine when used just for testing.
@@ -11,4 +14,4 @@ print ("headers:")
 print (" ", "\n  ".join (str (h) for h in resp.headers.items ()))
 print ("list of systems returned by API:")
 ret = resp.json ()
-print (" ", "\n  ".join (("  ".join (f for f in l) for l in ret)))
+print (" ", "\n  ".join (ret))
